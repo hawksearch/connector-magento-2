9ca3039356f22ec74c761dad67be27a023bb29a4
Alexander Martinkevich <amartinkevich@bridgeline.com>
2025-01-17 14:16:18 +0300

    feat: add PublicPropertyDeprecationTrait

--- Compatibility/DeprecatedMessageInterface.php
+++ Compatibility/DeprecatedMessageInterface.php
@@ -23,12 +23,13 @@ interface DeprecatedMessageInterface
      * Deprecated message templates
      */
     const TEMPLATE_SINCE_PART = 'Since %s:';
-    const TEMPLATE_PUBLIC_METHOD_MAIN_PART = 'Method %s has been deprecated and it\'s public usage will be discontinued.';
+    const TEMPLATE_PUBLIC_METHOD_MAIN_PART = 'Method %s has been deprecated and it\'s public/protected usage will be discontinued.';
     const TEMPLATE_DERIVED_METHOD_MAIN_PART = 'Inheritance of %s method is deprecated because of deprecation of the base method.';
     const TEMPLATE_NEW_METHOD_MAIN_PART = 'Class %s is deprecated without %s method.';
     const TEMPLATE_INTERFACE_MAIN_PART = 'Interface %s has been deprecated and will be removed.';
     const TEMPLATE_CLASS_MAIN_PART = 'Class %s has been deprecated and will be removed.';
     const TEMPLATE_PARAMETER_MAIN_PART = 'Parameter %s has been deprecated in method %s and will be removed.';
+    const TEMPLATE_PUBLIC_PROPERTY_MAIN_PART = 'Property %s has been deprecated and it\'s public/protected usage will be discontinued.';
     const TEMPLATE_REPLACEMENT_PART = 'Use %s instead.';
     /**#@-*/
 
---
+++ Test/Unit/Compatibility/Fixtures/PublicPropertyDeprecationFixture.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright (c) 2025 Hawksearch (www.hawksearch.com) - All Rights Reserved
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+declare(strict_types=1);
+
+namespace HawkSearch\Connector\Test\Unit\Compatibility\Fixtures;
+
+use HawkSearch\Connector\Compatibility\PublicPropertyDeprecationTrait;
+
+class PublicPropertyDeprecationBase
+{
+    use PublicPropertyDeprecationTrait;
+
+    private array $deprecatedPublicProperties = [];
+
+    /**
+     * @deprecated
+     */
+    private string $propertyDeprecatedChangedToPrivateScope = 'initial private value';
+
+    /**
+     * @deprecated
+     */
+    protected string $propertyDeprecatedChangedToProtectedScope = 'initial protected value';
+
+    protected string $regularProtectedProperty = 'regular';
+
+    private string $regularPrivateProperty = 'regular';
+
+    public function __construct(array $deprecatedPublicProperties)
+    {
+        $this->deprecatedPublicProperties = $deprecatedPublicProperties;
+    }
+
+    public function accessDeprecatedPrivateFromPublicMethodNoDeprecationNotice(): string
+    {
+        return $this->propertyDeprecatedChangedToPrivateScope;
+    }
+
+    public function accessDeprecatedProtectedFromPublicMethodNoDeprecationNotice(): string
+    {
+        return $this->propertyDeprecatedChangedToProtectedScope;
+    }
+}
+
+class PublicPropertyDeprecationDerived extends PublicPropertyDeprecationBase
+{
+    public function accessRegularProtectedPropertyFromDerivedClassNoDeprecationNotice(): string
+    {
+        return $this->regularProtectedProperty;
+    }
+
+    public function accessDeprecatedPrivatePropertyFromDerivedClass(): string
+    {
+        return $this->propertyDeprecatedChangedToPrivateScope;
+    }
+
+    public function accessDeprecatedProtectedPropertyFromDerivedClassNoDeprecationNotice(): string
+    {
+        return $this->propertyDeprecatedChangedToProtectedScope;
+    }
+
+    public function setProperty(string $property, mixed $value): void
+    {
+        $this->$property = $value;
+    }
+}
